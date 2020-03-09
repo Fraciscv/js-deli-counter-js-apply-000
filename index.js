7b3203e711cfd2a3bb5418a9f4ac944ff68f85a4
Francisco Andaur <francisco.andaur@icloud.com>
2020-03-09 12:40:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,30 +3,45 @@ function takeANumber(lineArray, nameString){
     for (let i = lineArray.length; i > 0; i++) {
       return `Welcome, ${nameString}. You are number ${i} in line.`;
     }
-
-
 }
 
-function nowServing(line){
-    if (line.length === 0) {
-      return 'There is nobody waiting to be served!'
-    } else {
-      return `Currently serving ${line.shift()}.`
-    }
-}
-
-function currentLine(line) {
-  if (line.length === 0) {
-      return `The line is currently empty.`;
+//function nowServing(line){
+//    if (line.length === 0) {
+//    return 'There is nobody waiting to be served!'
+//    } else {
+//      return `Currently serving ${line.shift()}.`
+//    }
+//}
+function nowServing(line) {
+  if (line.length > 0) {
+  return `Currently serving ${line.shift()}.`
   } else {
-      const currentLine = []
-
-      for(let i = 0; i < line.length; i++) {
-        currentLine.push(` ${i + 1}. ${line[i]}`)
-      }
-
-  return `The line is currently:${currentLine}`
+    return 'There is nobody waiting to be served!'
   }
 }
 
-console.log("I would rather go down the street to Russ & Daughters.")
+//function currentLine(line) {
+//  if (line.length === 0) {
+//      return `The line is currently empty.`;
+//  } else {
+//      const currentLine = []
+//      for(let i = 0; i < line.length; i++) {
+//        currentLine.push(` ${i + 1}. ${line[i]}`)
+//      }
+
+//  return `The line is currently:${currentLine}`
+//  }
+//}
+
+function currentLine(line) {
+  const currentLine = []
+
+  if (line.length > 0) {
+    for(let i = 0; i < line.length; i++) {
+        currentLine.push(` ${i + 1}. ${line[i]}`)
+      }
+      return `The line is currently:${currentLine}`
+}   else {
+  return `The line is currently empty.`;
+}
+}
